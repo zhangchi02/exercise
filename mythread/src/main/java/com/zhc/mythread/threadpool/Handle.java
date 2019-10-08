06de75e49fa79f15ecc38dafe9aeeb75a905abb7
zhangchi02 <1466504436@qq.com>
2019-10-08 19:15:35 +0800

    线程池练习

--- mythread/src/main/java/com/zhc/mythread/threadpool/Handle.java
+++ mythread/src/main/java/com/zhc/mythread/threadpool/Handle.java
@@ -45,7 +45,7 @@ class Handle implements Runnable {
 //		testSingleThreadPool();
 		testScheduledThreadPool();
 	}
-	
+
 	public static void testCachedThreadPool() {
 		System.out.println("Main: Starting at: " + new Date());
 		ExecutorService exec = Executors.newCachedThreadPool(); // 创建一个缓冲池，缓冲池容量大小为Integer.MAX_VALUE
@@ -58,34 +58,34 @@ class Handle implements Runnable {
 
 	public static void testFixThreadPool() {
 		System.out.println("Main Thread: Starting at: " + new Date());
-		ExecutorService exec = Executors.newFixedThreadPool(5);	
+		ExecutorService exec = Executors.newFixedThreadPool(5);
 		for (int i = 0; i < 10; i++) {
 			exec.execute(new Handle(String.valueOf(i)));
 		}
 		exec.shutdown(); // 执行到此处并不会马上关闭线程池
 		System.out.println("Main Thread: Finished at:" + new Date());
 	}
-	
+
 	public static void testSingleThreadPool() {
-		 System.out.println("Main Thread: Starting at: "+ new Date());  
-		 ExecutorService exec = Executors.newSingleThreadExecutor();   //创建大小为1的固定线程池
-	     for(int i = 0; i < 10; i++) {   
-	            exec.execute(new Handle(String.valueOf(i)));   
-	     }   
-	     exec.shutdown();  //执行到此处并不会马上关闭线程池
-	     System.out.println("Main Thread: Finished at:"+ new Date());
+		System.out.println("Main Thread: Starting at: " + new Date());
+		ExecutorService exec = Executors.newSingleThreadExecutor(); // 创建大小为1的固定线程池
+		for (int i = 0; i < 10; i++) {
+			exec.execute(new Handle(String.valueOf(i)));
+		}
+		exec.shutdown(); // 执行到此处并不会马上关闭线程池
+		System.out.println("Main Thread: Finished at:" + new Date());
 	}
-	
+
 	public static void testScheduledThreadPool() {
-		System.out.println("Main Thread: Starting at: "+ new Date());  
-		ScheduledThreadPoolExecutor  exec = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(10);   //创建大小为10的线程池
-	     for(int i = 0; i < 10; i++) {   
-	            exec.schedule(new Handle(String.valueOf(i)), 10, TimeUnit.SECONDS);//延迟10秒执行
-	     }   
-	     exec.shutdown();  //执行到此处并不会马上关闭线程池
-	     while(!exec.isTerminated()){
-	            //wait for all tasks to finish
-	     }
-	     System.out.println("Main Thread: Finished at:"+ new Date());
+		System.out.println("Main Thread: Starting at: " + new Date());
+		ScheduledThreadPoolExecutor exec = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(10); // 创建大小为10的线程池
+		for (int i = 0; i < 10; i++) {
+			exec.schedule(new Handle(String.valueOf(i)), 10, TimeUnit.SECONDS);// 延迟10秒执行
+		}
+		exec.shutdown(); // 执行到此处并不会马上关闭线程池
+//		while (!exec.isTerminated()) {
+//			// wait for all tasks to finish
+//		}
+		System.out.println("Main Thread: Finished at:" + new Date());
 	}
 }
